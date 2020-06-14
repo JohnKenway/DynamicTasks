ad887e3cc055f369af40e6a02d408f53ff006345
JohnKenway <csh865177145@hotmail.com>
2020-06-15 02:30:56 +0800

    JohnKenway Task#0001 Create project and upload
    Create project and upload

---
+++ src/main/java/org/swstudio/demo/dynamictasks/DynamictasksApplication.java
@@ -0,0 +1,13 @@
+package org.swstudio.demo.dynamictasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamictasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamictasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/swstudio/demo/dynamictasks/DynamictasksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swstudio.demo.dynamictasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamictasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
